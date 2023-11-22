4c795485f38a1aa5bea09d6dfae269b6126adffe
Moh-Zaman <145913815+Moh-Zaman@users.noreply.github.com>
2023-11-22 10:28:32 +0000

    Added in button function, search option and displaying weather in correct columns. Fully functional with limited features

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,113 @@
-// API Setup
+// Array for Storage
+cityNames = ["London", "Melbourne", "Tokyo"]
 
-var APIKey = "3ae9b9a3bec3ebb3884cc2d6643eedaf"
+// Displaying Weather Function
+function displayWeather () {
 
+    // API Setup
+    var APIKey = "3ae9b9a3bec3ebb3884cc2d6643eedaf"
+    var cityName = $(this).attr("data-city")
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=metric&appid=" + APIKey
 
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=London&units=metric&appid=" + APIKey
+    fetch(queryURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
 
-cityName = []
+        console.log(data)
 
-fetch(queryURL)
-.then(function (response) {
-    return response.json();
+        $("#currentDay").empty();
+        $("#fourDay").empty();
+
+        searchedName = data.city.name
+
+        pZero = $("#currentDay-text").text("Current day in " + searchedName + ":");
+
+        // Looping API for Data
+        for (var i = 8; i < 33; i += 8) {
+
+            //Time
+            var currentTime = data.list[0].dt_txt
+
+            var time = data.list[i].dt_txt
+
+            var now = dayjs(currentTime).format("dddd")
+
+            var after = dayjs(time).format("dddd")
+
+            pOne = $("<p>").text(now)
+      
+            // Temp
+            var currentTemp = data.list[0].main.temp
+
+            var temp = data.list[i].main.temp
+            
+            pTwo = $("<p>").text("Temperature: " + currentTemp + " °C")
+
+            // Wind
+            var currentWind = data.list[0].wind.speed
+
+            var wind = data.list[i].wind.speed
+
+            pThree = $("<p>").text("Wind Speeds: " + currentWind + " KPH")
+
+            //Humidity
+            var currentHum = data.list[0].main.humidity
+
+            var hum = data.list[i].main.humidity
+
+            pFour = $("<p>").text("Humidity: " + currentHum + "%")
+
+            // The next 4 days
+            fourTime = $("<p>").text(after)
+            fourTemp = $("<p>").text("Temperature: " + temp + " °C")
+            fourWind = $("<p>").text("Wind Speeds: " + wind + " KPH")
+            fourHum = $("<p>").text("Humidity: " + hum + "%")
+
+            var four = $("#fourDay")
+
+            var fourDiv = $("<div class='col card'>")
+
+            fourDiv.append(fourTime, fourTemp, fourWind, fourHum)
+
+            four.append(fourDiv)
+
+        } 
+
+        currentDay = $("#currentDay")
+
+        currentDay.append(pOne, pTwo, pThree, pFour)
+        
+    })
+}
+
+//Search History Buttons
+function historyButtons() {
+
+    $("#citySearched").empty();
+
+    for (var i = 0; i < cityNames.length; i++) {
+        var a = $("<button type='button' class='btn btn-secondary w-100 btnSpace' id='cities'>")
+
+        a.attr("data-city", cityNames[i]);
+
+        a.text(cityNames[i])
+
+        $("#citySearched").append(a)
+    }
+}
+
+$("#searchButton").on("click", function(event) {
+    event.preventDefault();
+
+    var city = $("#inputBar").val().trim();
+
+    cityNames.push(city);
+
+    historyButtons();
 })
-.then(function (data) {
-    
-    console.log(data)
 
-    searchedName = data.city.name
+$(document).on("click", "#cities", displayWeather);
 
-    pZero = $("<p>").text("City: " + searchedName);
-
-    // Temp
-    for (var i = 8; i < 33; i += 8) {
-
-        var currentTemp = data.list[0].main.temp
-
-        var temp = data.list[i].main.temp
-        
-        pOne = $("<p>").text("Temperature: " + currentTemp + " Celsius")
-
-        
-    }
-
-
-
-    // Wind
-    for (var j = 8; j < 33; j += 8) {
-
-        var currentWind = data.list[0].wind.speed
-
-        var wind = data.list[j].wind.speed
-
-        pTwo = $("<p>").text("Wind Speeds: " + currentWind + " KPH")
-    }
-        
-
-    //Humidity
-    for (var k = 8; k < 33; k += 8) {
-
-        var currentHum = data.list[0].main.humidity
-
-        var hum = data.list[k].main.humidity
-
-        pThree = $("<p>").text("Humidity: " + currentHum + "%")
-
-        
-    }
-
-
-    currentDay = $("#currentDay")
-
-    currentDay.append(pZero, pOne, pTwo, pThree)
-
-
-
-    
-
-
-
-})
-
+historyButtons();
